0fc63faa7fc04afb7893119cd758adaeb9fcc5fc
Pouya Ghazvini <pouyaghazvini@gmail.com>
2019-05-23 03:44:01 -0700

    finished step 0

--- movie-list/src/components/App.js
+++ movie-list/src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Movie List App</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App' style={{border: '2px solid lightgray', width: '60%'}}>
+       
+       <div className='App-Header'>
+         <h1>Movie List</h1>
+       </div>
+      
+       <div>
+         <List/>
+       </div>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
--- movie-list/src/components/List.js
+++ movie-list/src/components/List.js
@@ -1,4 +1,36 @@
-var movies = [
+import React from 'react';
+
+
+const movies = [
   {title: 'My Cousin Vinny'},
-  {title: ''}
-]
+  {title: 'Man on Fire'},
+  {title: 'Goodfellas'},
+  {title: 'Lion King'},
+  {title: 'Robin Hood'}
+];
+
+class List extends React.Component {
+  render() {
+    return (
+      <table id='movie-list'>
+        <tr>
+          <td>{movies[0].title}</td>  
+        </tr>
+        <tr>
+          <td>{movies[1].title}</td>  
+        </tr>
+        <tr>
+          <td>{movies[2].title}</td>  
+        </tr>
+        <tr>
+          <td>{movies[3].title}</td>  
+        </tr>
+        <tr>
+          <td>{movies[4].title}</td>  
+        </tr>
+      </table>
+    )
+  }
+}
+
+export default List;
